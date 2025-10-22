77079f0b462f98c43859ae47390180e6debbbbf7
Develop and System <developandsystem@gmail.com>
2025-10-22 22:01:38 +0200

    update

---
+++ PMDM/curso2526/T2/G2/Inicial/app/src/main/java/com/example/inicial/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.inicial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inicial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+    }
+
+
+}
